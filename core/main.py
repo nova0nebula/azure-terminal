6faf448789298e23679223db0f6ae6b5c3ed5a04
nova0nebula <CosmicNovaNebula@gmail.com>
2025-04-23 10:13:01 +0800

    Create main.py

---
+++ core/main.py
@@ -0,0 +1,16 @@
+# System Library Importing
+from system_library import Colour
+
+# Local Importing
+from azure_terminal.core.command_handling import execute_command
+from azure_terminal.config.config_handler import prompt_colour
+
+# Main Function
+def azure_terminal_main():
+    while True:
+        prompt: str = str(input(f"{prompt_colour}azure-core$ {Colour.RESET}")).strip()
+        execute_command(prompt)
+
+# Calling main function
+if __name__ == "__main__":
+    azure_terminal_main()
